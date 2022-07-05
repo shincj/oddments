6a345a57537632a6fb1a931bb58171e07e764925
shincj <zulaman@naver.com>
2022-07-05 14:47:25 +0900

    emptyhead.h

--- asd.c
+++ asd.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-	printf("zzz\n");
+	printf("baboya\n");
 	return 0;
 }
---
+++ emptyhead.h
@@ -0,0 +1,13 @@
+#ifndef __EMPTYHEAD_H__
+#define __EMPTYHEAD_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <complex.h>
+#include <math.h>
+#include <ctype.h>
+
+#endif
